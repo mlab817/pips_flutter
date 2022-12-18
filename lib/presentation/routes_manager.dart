c9456e355a2104c960a8a60a007f26dd3317f081
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-12-18 17:12:52 +0800

    added routes and strings manager

--- lib/presentation/routes_manager.dart
+++ lib/presentation/routes_manager.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pips_flutter/presentation/login/login.dart';
+import 'package:pips_flutter/presentation/onboarding/onboarding.dart';
+import 'package:pips_flutter/presentation/splash/splash.dart';
+import 'package:pips_flutter/presentation/strings_manager.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String onBoardingRoute = "/onboarding";
@@ -6,4 +13,31 @@ class Routes {
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const OnboardingView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text(AppStrings.noRouteFound),
+              ),
+              body: const Center(
+                child: Text(AppStrings.noRouteFound),
+              ),
+            ));
+  }
 }
